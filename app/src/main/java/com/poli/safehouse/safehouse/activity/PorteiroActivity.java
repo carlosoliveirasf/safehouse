465f10324cb90a4f0a4006e85dc6db8302409bb4
carlos <carlos.felipe@segsat.com>
2018-07-25 16:33:29 -0300

    abhdsdasd

---
+++ app/src/main/java/com/poli/safehouse/safehouse/activity/PorteiroActivity.java
@@ -0,0 +1,15 @@
+package com.poli.safehouse.safehouse.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.poli.safehouse.safehouse.R;
+
+public class PorteiroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_porteiro);
+    }
+}
---
+++ app/src/main/java/com/poli/safehouse/safehouse/activity/UsuarioActivity.java
@@ -0,0 +1,13 @@
+package com.poli.safehouse.safehouse.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+    }
+}
