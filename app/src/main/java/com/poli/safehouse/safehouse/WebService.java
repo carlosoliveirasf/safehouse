1e154283c03baa9269318a3e00517f725f787f27
carlos <carlos.felipe@segsat.com>
2018-07-26 08:53:03 -0300

    Login fix

--- app/src/main/java/com/poli/safehouse/safehouse/WebService.java
+++ app/src/main/java/com/poli/safehouse/safehouse/WebService.java
@@ -4,16 +4,17 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface WebService {
     String BASE_URL = "http://10.0.3.155:8077/";
-    String VALIDAR_LOGIN = "login/";
+    String VALIDAR_LOGIN = "user/login";
 
     @POST(VALIDAR_LOGIN)
     Call<Void> getUser(@Body PedidoAutenticacao pedidoAutenticacao);
 
-    @FormUrlEncoded
-    @POST(VALIDAR_LOGIN)
-    Call<Void> getlogin(@Field("login") String login, @Field("password") String password);
+    @GET(VALIDAR_LOGIN)
+    Call<Void> getlogin(@Query("login") String login, @Query("password") String password);
 }
